73ab4b8ca6cf0a5fb6765038a925f953febcd934
Zachary Seguin <development@zacharyseguin.ca>
2014-02-13 18:31:03 -0500

    Updated Test

--- CanadaAlertSystem/CanadaAlertSystemTesting/Program.cs
+++ CanadaAlertSystem/CanadaAlertSystemTesting/Program.cs
@@ -5,9 +5,12 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.IO;
+using System.Net.Sockets;
+using System.Net;
 using System.Xml.Linq;
 
 using ZacharySeguin.CanadaAlertSystem;
+using System.Diagnostics;
 
 namespace ZacharySeguin.CanadaAlertSystemTesting
 {
@@ -19,16 +22,29 @@ namespace ZacharySeguin.CanadaAlertSystemTesting
 
             system.AlertReceived += delegate(object sender, AlertEventArgs args)
             {
+                if (args.Alert.Status != AlertStatus.Actual && args.Alert.Status != AlertStatus.Exercise)
+                {
+                    Debug.WriteLine("{0} - Non actual/exercise alert received.", DateTime.Now.ToString());
+                    return;
+                }
+
                 Console.WriteLine("*** ALERT ****\n{0}: {1} at {2}", args.Alert.Sender, args.Alert.Identifier, args.Alert.Sent.ToString());
 
                 foreach (AlertInfo info in args.Alert.Information)
                 {
                     Console.WriteLine("\t{0} - {1} at {2}", info.SenderName, info.Event, info.Effective.ToString());
+
+                    foreach (AlertArea area in info.Areas)
+                    {
+                        Console.WriteLine("\t\tEffective For: " + area.Description);
+                        Console.WriteLine(area.Polygons[0]);
+                    }// End of foreach
                 }// End of foreach
+
+                Console.WriteLine();
             };
-            
-            XDocument xDoc = XDocument.Load(@"C:\Users\zachary\Documents\Visual Studio 2013\Projects\CanadaAlertingSystem\CanadaAlertingSystem\ExampleXML\AlertExample1.xml");
-            Console.WriteLine("Success: " + system.LoadFromXmlDocument(xDoc));
+
+            system.ConnectToStream("streaming1.naad-adna.pelmorex.com", 8080);
 
             Console.Read();
         }
