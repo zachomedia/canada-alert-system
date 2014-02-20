20557fdac029a502576b0c51545548bc313d078c
Zachary Seguin <development@zacharyseguin.ca>
2014-02-19 23:32:53 -0500

    Updates to testing

--- CanadaAlertSystem/CanadaAlertSystemTesting/Program.cs
+++ CanadaAlertSystem/CanadaAlertSystemTesting/Program.cs
@@ -22,9 +22,11 @@ namespace ZacharySeguin.CanadaAlertSystemTesting
 
             system.AlertReceived += delegate(object sender, AlertEventArgs args)
             {
+                args.Alert.ToXmlFile(@"C:\TEMP\ALERTS\alert-" + args.Alert.Identifier + ".xml");
+
                 if (args.Alert.Status != AlertStatus.Actual && args.Alert.Status != AlertStatus.Exercise)
                 {
-                    Debug.WriteLine("{0} - Non actual/exercise alert received.", DateTime.Now.ToString());
+                    Debug.WriteLine("Non actual/exercise alert received.", DateTime.Now.ToString());
                     return;
                 }
 
@@ -33,11 +35,14 @@ namespace ZacharySeguin.CanadaAlertSystemTesting
                 foreach (AlertInfo info in args.Alert.Information)
                 {
                     Console.WriteLine("\t{0} - {1} at {2}", info.SenderName, info.Event, info.Effective.ToString());
+                    Console.WriteLine("\t{0}", info.Headline.ToUpper());
+                    Console.WriteLine("\tDecription: {0}", info.Description);
+                    Console.WriteLine("\tAction: {0}", info.Instruction);
 
                     foreach (AlertArea area in info.Areas)
                     {
                         Console.WriteLine("\t\tEffective For: " + area.Description);
-                        Console.WriteLine(area.Polygons[0]);
+                        //Console.WriteLine(area.Polygons[0]);
                     }// End of foreach
                 }// End of foreach
 
