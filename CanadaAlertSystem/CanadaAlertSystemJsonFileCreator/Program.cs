de81f9663ce588ba70fae8f022c24c1fc673b04c
Zachary Seguin <development@zacharyseguin.ca>
2014-02-20 22:18:59 -0500

    Re-added deleting of expired alerts and added reload capabilities to JSON File Creator

--- CanadaAlertSystem/CanadaAlertSystemJsonFileCreator/Program.cs
+++ CanadaAlertSystem/CanadaAlertSystemJsonFileCreator/Program.cs
@@ -22,18 +22,33 @@ namespace ZacharySeguin.CanadaAlertSystemJsonFileCreator
         /// <param name="filename"></param>
         public static void CreateJsonFile(string filename)
         {
-            using (StreamWriter file = new StreamWriter(filename))
+            for (int x = 0; x < 10; ++x)
             {
-                file.WriteLine(JsonConvert.SerializeObject(AlertSystem.GetAlerts()));
-            }// End of using
+                try
+                {
+                    using (StreamWriter file = new StreamWriter(filename))
+                    {
+                        file.WriteLine(JsonConvert.SerializeObject(AlertSystem.GetAlerts()));
+                    }// End of using
+
+                    break;
+                }// End of try
+                catch (Exception)
+                {
+                    System.Threading.Thread.Sleep(500);
+                }// End of catch
+            }// End of for
         }// End of CreateJsonFile method
 
         static void Main(string[] args)
         {
+            // Filename
             string filename = "alerts.json";
             if (args.Length != 0)
                 filename = args[0];
 
+            // Create Alert System
+            // and register for events.
             AlertSystem = new AlertSystem();
 
             AlertSystem.AlertReceived += delegate(object sender, AlertEventArgs e)
@@ -51,14 +66,35 @@ namespace ZacharySeguin.CanadaAlertSystemJsonFileCreator
                 CreateJsonFile(filename);
             };
 
-            // FOR TESTING ONLY
-            foreach (string file in System.IO.Directory.GetFiles(@"C:\TEMP\Alerts\"))
-                AlertSystem.LoadFromXmlSerializationFile(file);
+            // If the file exists, load existing alerts
+            //      If they have expired, they will automatically be removed.
+            try
+            {
+                if (File.Exists(filename))
+                {
+                    List<Alert> alerts = new List<Alert>();
 
+                    using (StreamReader reader = new StreamReader(filename))
+                    {
+                        alerts = (List<Alert>)JsonConvert.DeserializeObject(reader.ReadToEnd(), typeof(List<Alert>));
+                    }// End of using
+
+                    alerts.ForEach(a => AlertSystem.AddAlert(a));
+                }// End of if
+            }// End of try
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }// End of catch
+
+            // Connect to the NAAD stream
             AlertSystem.ConnectToStream("streaming1.naad-adna.pelmorex.com", 8080);
 
             while (true)
+            {
                 System.Threading.Thread.Sleep(5000);
+            }// End of while
         }// End of main method
     }// End of class
 }// End of namespace
